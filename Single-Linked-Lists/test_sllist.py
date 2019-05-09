a8c063e8e11b0ba8a017537d6e6f5566dc454e56
xia0m <alexma325@gmail.com>
2019-05-08 21:02:18 -0700

    added test file

---
+++ Single-Linked-Lists/test_sllist.py
@@ -0,0 +1,9 @@
+from sllist import *
+
+
+def test_push():
+    colors = SingleLinkedList()
+    colors.push("Pthalo Blue")
+    assert colors.count() == 1
+    colors.push("Ultramarine Blue")
+    assert colors.count() == 2
