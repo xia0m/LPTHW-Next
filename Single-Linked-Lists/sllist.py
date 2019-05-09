59c65ace8471b8689642006f6c7dc105716acde0
xia0m <alexma325@gmail.com>
2019-05-08 21:42:27 -0700

    added push,pop, shift and their test cases

--- Single-Linked-Lists/sllist.py
+++ Single-Linked-Lists/sllist.py
@@ -17,12 +17,37 @@ class SingleLinkedList(object):
 
     def push(self, obj):
         """Appends a new value on the end of the list"""
+        if self.begin is None:
+            self.begin = SingleLinkedListNode(obj, None)
+            return
+        node = self.begin
+        while node.next:
+            node = node.next
+        node.next = SingleLinkedListNode(obj, None)
 
     def pop(self):
         """Removes the last item and returns it ."""
+        if self.begin is None:
+            return None
+        if self.begin.next is None:
+            result = self.begin.value
+            self.begin = None
+            return result
+        node = self.begin
+        while node.next.next:
+            node = node.next
+        result = node.next.value
+        node.next = None
+        return result
 
     def shift(self, obj):
         """Another name for push."""
+        if self.begin is None:
+            self.begin = SingleLinkedListNode(obj, None)
+            return
+        temp = self.begin
+        self.begin = SingleLinkedListNode(obj, None)
+        self.begin.next = temp
 
     def unshift(self):
         """Removes the first item and returns it."""
@@ -38,9 +63,27 @@ class SingleLinkedList(object):
 
     def count(self):
         """Counts the number of elements in the list."""
+        if self.begin is None:
+            return 0
+        node = self.begin
+        count = 0
+        while node:
+            count += 1
+            node = node.next
+        return count
 
     def get(self, index):
         """Get the value at index."""
 
     def dump(self, mark):
         """Debugging function that dumps the contents of the list."""
+
+    def to_list(self):
+        if self.begin is None:
+            return []
+        a_list = []
+        node = self.begin
+        while node:
+            a_list.append(node.value)
+            node = node.next
+        return a_list
--- Single-Linked-Lists/test_sllist.py
+++ Single-Linked-Lists/test_sllist.py
@@ -7,3 +7,33 @@ def test_push():
     assert colors.count() == 1
     colors.push("Ultramarine Blue")
     assert colors.count() == 2
+
+
+def test_pop():
+    colors = SingleLinkedList()
+    colors.push("Magenta")
+    colors.push("Alizarin")
+    assert colors.pop() == "Alizarin"
+    # colors.pop()
+    # print(colors.to_list())
+    assert colors.pop() == "Magenta"
+    assert colors.pop() == None
+
+
+def test_shift():
+    colors = SingleLinkedList()
+    colors.shift("Cadmium Orange")
+    assert colors.count() == 1
+
+    colors.shift("Carbazole Violet")
+    assert colors.count() == 2
+
+    assert colors.pop() == "Cadmium Orange"
+    assert colors.count() == 1
+    assert colors.pop() == "Carbazole Violet"
+    assert colors.count() == 0
+
+
+test_push()
+test_pop()
+test_shift()
