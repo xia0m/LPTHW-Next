4eb80642827c7bde77d9539cbd7e4160aab68417
xia0m <alexma325@gmail.com>
2019-05-15 15:47:01 -0700

    panlindrome using recursion

---
+++ Recursion/panlindrome.py
@@ -0,0 +1,22 @@
+def is_palindrome(input):
+    """
+    Return True if input is palindrome, False otherwise.
+
+    Args:
+       input(str): input to be checked if it is palindrome
+    """
+
+    if len(input) <= 1:
+        return True
+    if input[0] == input[-1]:
+        is_palindrome(input[1:-1])
+        return True
+    else:
+        return False
+
+
+print("Pass" if (is_palindrome("")) else "Fail")
+print("Pass" if (is_palindrome("a")) else "Fail")
+print("Pass" if (is_palindrome("madam")) else "Fail")
+print("Pass" if (is_palindrome("abba")) else "Fail")
+print("Pass" if not (is_palindrome("Udacity")) else "Fail")
