9cb3476fc2cb3a45325328b39e5f78758988b6df
xia0m <alexma325@gmail.com>
2019-05-28 21:38:22 -0700

    Huffman Decoding

--- Data_Structure_Problems/Huffman_Coding.py
+++ Data_Structure_Problems/Huffman_Coding.py
@@ -13,6 +13,21 @@ class Node(object):
 def huffman_encoding(data):
     char_dict = count_char(data)
     freq_list = tuple_list(char_dict)
+    build_huffman_tree(freq_list)
+    huffman_tree = freq_list[0][1]
+
+    for key in char_dict:
+        char_dict[key] = get_binary_value(key, huffman_tree)
+
+    encoded_data = ""
+
+    for character in data:
+        encoded_data += char_dict[character]
+
+    return encoded_data, huffman_tree
+
+
+def build_huffman_tree(freq_list):
     while(len(freq_list) > 1):
         leftValue = freq_list[0][1]
         leftFrequency = freq_list[0][0]
@@ -33,18 +48,8 @@ def huffman_encoding(data):
 
         freq_list.append((totalFrequency, tree))
         del freq_list[:2]
-        # if len(freq_list) == 2:
-        #     continue
+
         freq_list.sort(key=lambda x: x[0])
-    # print(len(freq_list))
-    huffman_tree = freq_list[0][1]
-    for key in char_dict:
-        char_dict[key] = get_binary_value(key, huffman_tree)
-    print(char_dict)
-    encoded_data = ""
-    for character in data:
-        encoded_data += char_dict[character]
-    print(encoded_data)
 
 
 def get_binary_value(key, tree):
@@ -86,31 +91,43 @@ def tuple_list(char_dict):
 
     return a_list
 
-
-huffman_encoding("aabbbbbcccdeeeeeee")
+# DECODING
 
 
 def huffman_decoding(data, tree):
-    pass
+    decoded_data = ""
+    if tree is None:
+        return ""
+    node = tree
+    for digit in data:
+        if node.value == '*':
+            if digit == '0':
+                node = node.left
+            elif digit == '1':
+                node = node.right
+        if node.value != '*':
+            decoded_data += node.value
+            node = tree
+    return decoded_data
 
 
-# if __name__ == "__main__":
-#     codes = {}
+if __name__ == "__main__":
+    codes = {}
 
-#     a_great_sentence = "The bird is the word"
+    a_great_sentence = "The bird is the word"
 
-#     print("The size of the data is: {}\n".format(
-#         sys.getsizeof(a_great_sentence)))
-#     print("The content of the data is: {}\n".format(a_great_sentence))
+    print("The size of the data is: {}\n".format(
+        sys.getsizeof(a_great_sentence)))
+    print("The content of the data is: {}\n".format(a_great_sentence))
 
-#     encoded_data, tree = huffman_encoding(a_great_sentence)
+    encoded_data, tree = huffman_encoding(a_great_sentence)
 
-#     print("The size of the encoded data is: {}\n".format(
-#         sys.getsizeof(int(encoded_data, base=2))))
-#     print("The content of the encoded data is: {}\n".format(encoded_data))
+    print("The size of the encoded data is: {}\n".format(
+        sys.getsizeof(int(encoded_data, base=2))))
+    print("The content of the encoded data is: {}\n".format(encoded_data))
 
-#     decoded_data = huffman_decoding(encoded_data, tree)
+    decoded_data = huffman_decoding(encoded_data, tree)
 
-#     print("The size of the decoded data is: {}\n".format(
-#         sys.getsizeof(decoded_data)))
-#     print("The content of the encoded data is: {}\n".format(decoded_data))
+    print("The size of the decoded data is: {}\n".format(
+        sys.getsizeof(decoded_data)))
+    print("The content of the encoded data is: {}\n".format(decoded_data))
