0bba366baba9afac8fa9dbcc202b67dc83c64383
xia0m <alexma325@gmail.com>
2019-06-29 23:04:07 -0700

    finished DFS

--- Graph/DFS.py
+++ Graph/DFS.py
@@ -41,3 +41,25 @@ graph1.add_edge(nodeR, nodeP)
 graph1.add_edge(nodeH, nodeG)
 graph1.add_edge(nodeH, nodeP)
 graph1.add_edge(nodeS, nodeR)
+
+
+def dfs_search(root_node, search_value):
+    visited = []
+
+    stack = [root_node]
+
+    while len(stack) > 0:
+        current_node = stack.pop()
+        visited.append(current_node)
+
+        if current_node.value == search_value:
+            return current_node
+
+        for child in current_node.children:
+            if child not in visited:
+                stack.append(child)
+
+
+assert nodeA == dfs_search(nodeS, 'A')
+assert nodeS == dfs_search(nodeP, 'S')
+assert nodeR == dfs_search(nodeH, 'R')
