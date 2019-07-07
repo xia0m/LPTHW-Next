7caf9118a02152681992cd73fcddc5d88cf3779b
xia0m <alexma325@gmail.com>
2019-07-07 16:09:35 -0700

    initial setup for connecting islands

---
+++ Graph/connecting_islands.py
@@ -0,0 +1,50 @@
+import heapq
+heap = list()
+heapq.heappush(heap, [1, 2, 1])
+heapq.heappush(heap, [2, 3, 4])
+
+print(heap)
+
+
+def get_minimum_cost_of_connecting(num_islands, bridge_config):
+    """
+    :param: num_islands - number of islands
+    :param: bridge_config - bridge configuration as explained in the problem statement
+    return: cost (int) minimum cost of connecting all islands
+    TODO complete this method to returh minimum cost of connecting all islands
+    """
+    pass
+
+
+def test_function(test_case):
+    num_islands = test_case[0]
+    bridge_config = test_case[1]
+    solution = test_case[2]
+    output = get_minimum_cost_of_connecting(num_islands, bridge_config)
+
+    if output == solution:
+        print("Pass")
+    else:
+        print("Fail")
+
+
+num_islands = 4
+bridge_config = [[1, 2, 1], [2, 3, 4], [1, 4, 3], [4, 3, 2], [1, 3, 10]]
+solution = 6
+
+test_case = [num_islands, bridge_config, solution]
+test_function(test_case)
+
+num_islands = 5
+bridge_config = [[1, 2, 5], [1, 3, 8], [2, 3, 9]]
+solution = 13
+
+test_case = [num_islands, bridge_config, solution]
+test_function(test_case)
+
+num_islands = 5
+bridge_config = [[1, 2, 3], [1, 5, 9], [2, 3, 10], [4, 3, 9]]
+solution = 31
+
+test_case = [num_islands, bridge_config, solution]
+test_function(test_case)
