9ad08a5a62acbce0e2e5e23378d506c1792332cf
xia0m <alexma325@gmail.com>
2019-07-08 11:33:53 -0700

    initial setup of knapsack problem

---
+++ Dynamic_Programming/knapsack.py
@@ -0,0 +1,27 @@
+import collections
+
+Item = collections.namedtuple('Item', ['weight', 'value'])
+
+
+def max_value(knapsack_max_weight, items):
+    """
+    Get the maximum value of the knapsack.
+    """
+    pass
+
+
+tests = [
+    {
+        'correct_output': 14,
+        'input':
+            {
+                'knapsack_max_weight': 15,
+                'items': [Item(10, 7), Item(9, 8), Item(5, 6)]}},
+    {
+        'correct_output': 13,
+        'input':
+            {
+                'knapsack_max_weight': 25,
+                'items': [Item(10, 2), Item(29, 10), Item(5, 7), Item(5, 3), Item(5, 1), Item(24, 12)]}}]
+for test in tests:
+    assert test['correct_output'] == max_value(**test['input'])
