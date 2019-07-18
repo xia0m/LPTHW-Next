6e8417acdc2ab2e7c7210115c409edf43e40d97f
xia0m <alexma325@gmail.com>
2019-07-18 13:31:08 -0700

    simplified knapsack

--- Dynamic_Programming/knapsack.py
+++ Dynamic_Programming/knapsack.py
@@ -4,19 +4,15 @@ Item = collections.namedtuple('Item', ['weight', 'value'])
 
 
 def max_value(knapsack_max_weight, items):
-    """
-    Get the maximum value of the knapsack.
-    """
-    sorted_items = sorted(items)
-    weight_dict = dict()
-    for weight, _ in sorted_items:
-        weight_dict[weight] = 0
-    for weight, value in sorted_items:
-        if knapsack_max_weight - weight >= 0:
-            knapsack_max_weight -= weight
-            for w in weight_dict:
-                weight_dict[w] += value
-    return max(weight_dict.items(), key=lambda x: x[1])[1]
+    lookup_table = [0] * (knapsack_max_weight + 1)
+
+    for item in items:
+        for capacity in reversed(range(knapsack_max_weight + 1)):
+            if item.weight <= capacity:
+                lookup_table[capacity] = max(
+                    lookup_table[capacity], lookup_table[capacity - item.weight] + item.value)
+
+    return lookup_table[-1]
 
 
 tests = [
