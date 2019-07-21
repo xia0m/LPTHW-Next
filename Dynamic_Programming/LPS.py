02c148e8cd9183dbfb9fb45b78e0539c87ad0cfe
xia0m <alexma325@gmail.com>
2019-07-21 01:04:22 -0700

    finished Longest palindromic sequence

--- Dynamic_Programming/LPS.py
+++ Dynamic_Programming/LPS.py
@@ -5,7 +5,22 @@ def lps(input_string):
 
     # TODO: Complete this implementation of the LPS function
     # The function should return one value: the LPS length for the given input string
-    pass
+    revere_string = input_string[::-1]
+    loopup_matrix = [[0]*(len(input_string)+1)
+                     for _ in range(len(input_string)+1)]
+    max = -1
+    for i in range(len(input_string)):
+        for j in range(len(input_string)):
+            if input_string[i] == revere_string[j]:
+                if i > 0 and j > 0:
+                    loopup_matrix[i][j] += loopup_matrix[i-1][j-1] + 1
+                else:
+                    loopup_matrix[i][j] += 1
+
+                if loopup_matrix[i][j] > max:
+                    max = loopup_matrix[i][j]
+
+    return max
 
 
 def test_function(test_case):
